ea8684fd6b29170fb53a4c883d16028596557b85
Juan Pablo Juliao <jpjuliao@gmail.com>
2024-03-28 18:01:25 -0500

    Configure storybook with SDC

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,16 @@
-import type { StorybookConfig } from "@storybook/html-vite";
-
-const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/html-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
+module.exports = {
+  stories: ['../components/**/*.stories.js'],
+  addons: [],
+  webpackFinal: async (config, { configType }) => {
+    // Add support for Twig files
+    config.module.rules.push({
+      test: /\.twig$/,
+      use: [
+        {
+          loader: 'twig-loader',
+        },
+      ],
+    });
+    return config;
   },
 };
-export default config;
