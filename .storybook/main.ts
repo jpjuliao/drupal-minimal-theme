d45c1fd5d1187750a08e6ed532f4575ea0d3ce82
Juan Pablo Juliao <jpjuliao@gmail.com>
2024-03-29 01:11:06 -0500

    Remove example stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,8 @@
 import type { StorybookConfig } from "@storybook/html-webpack5";
+import path from 'path'; 
 
 const config: StorybookConfig = {
   stories: [
-    // "../stories/**/*.mdx",
-    // "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
@@ -15,10 +14,18 @@ const config: StorybookConfig = {
     "@storybook/addon-mdx-gfm"
   ],
   webpackFinal: async (config, { configType }) => {
+
     // Add twig-loader rule for Twig files
     config.module.rules.push({
       test: /\.twig$/,
       use: 'twig-loader',
+    });
+
+    // Add CSS support
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+      include: path.resolve(__dirname, '../'),
     });
 
     // Return the updated Storybook Webpack config
